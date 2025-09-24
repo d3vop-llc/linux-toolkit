4bb500c5b121a598c681ecc9d6cb9442d775b98c
Caden F <121118774+MysticalMike60t@users.noreply.github.com>
2025-09-23 19:50:42 -0700

    Fixed "(" unexpected error

--- scripts/system/update_upgrade.sh
+++ scripts/system/update_upgrade.sh
@@ -1,77 +1,95 @@
 #!/bin/bash
 
-echo "Please select an option:"
-options=("Apt" "Pacman" "Dnf" "Yum" "Zypper" "Apk" "Emerge" "Xbps" "Eopkg" "Slackpkg" "Nix-channel + Nix-env" "Nixos-Rebuild" "Exit")
-
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Apt")
+while true; do
+    echo "Update Packages"
+    echo ""
+    echo "Please choose your package manager: "
+    echo ""
+    echo "1) Apt"
+    echo "2) Pacman"
+    echo "3) Dnf"
+    echo "4) Yum"
+    echo "5) Zypper"
+    echo "6) Apk"
+    echo "7) Emerge"
+    echo "8) Xbps"
+    echo "9) Eopkg"
+    echo "10) Slackpkg"
+    echo "11) Nix-channel + Nix-env"
+    echo "12) Nixos-Rebuild"
+    echo "13) Back"
+    echo ""
+    read -p "Please enter your choice: " choice
+    
+    case $choice in
+        1)
             echo "Updating system..."
             sudo apt update && sudo apt full-upgrade -y
             break
             ;;
-        "Pacman")
+        2)
             echo "Updating system..."
             sudo pacman -Syu --noconfirm
             break
             ;;
-        "Dnf")
+        3)
             echo "Updating system..."
             sudo dnf upgrade --refresh -y
             break
             ;;
-        "Yum")
+        4)
             echo "Updating system..."
             sudo yum update -y
             break
             ;;
-        "Zypper")
+        5)
             echo "Updating system..."
             sudo zypper refresh && sudo zypper update -y
             break
             ;;
-        "Apk")
+        6)
             echo "Updating system..."
             sudo apk update && sudo apk upgrade
             break
             ;;
-        "Emerge")
+        7)
             echo "Updating system..."
             sudo emerge --sync && sudo emerge -uUD @world
             break
             ;;
-        "Xbps")
+        8)
             echo "Updating system..."
             sudo xbps-install -Suv
             break
             ;;
-        "Eopkg")
+        9)
             echo "Updating system..."
             sudo eopkg upgrade
             break
             ;;
-        "Slackpkg")
+        10)
             echo "Updating system..."
             sudo slackpkg update && sudo slackpkg upgrade-all
             break
             ;;
-        "Nix-channel + Nix-env")
+        11)
             echo "Updating system..."
             sudo nix-channel --update && sudo nix-env -u --always
             break
             ;;
-        "Nixos-Rebuild")
+        12)
             echo "Updating system..."
             sudo nixos-rebuild switch --upgrade
             break
             ;;
-        "Exit")
-            echo "Goodbye!"
-            exit 0
+        13)
+            sudo sh ./start.sh
+            break
             ;;
-        *) 
+        *)
             echo "Invalid option. Please try again."
+            sleep 1
+            clear
             ;;
     esac
 done
